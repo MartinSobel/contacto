007c5221720946d6024788e04db9cdd5b2240e3a
Martín Sobel <marto.sobel@gmail.com>
2025-05-21 20:56:12 -0300

    ignores

--- face-detection.py
+++ face-detection.py
@@ -1,5 +1,5 @@
-import cv2
-import mediapipe as mp 
+import cv2 # type: ignore
+import mediapipe as mp  # type: ignore
 
 class Webcam:
     """Responsible for capturing frames from a USB webcam with configurable resolution"""
